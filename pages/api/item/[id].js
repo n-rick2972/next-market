988accacdb7839321a1b85b6108fca53ef014a8d
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-03-22 19:32:12 +0900

    ✨ アイテム（1つだけ）の読み取り機能

---
+++ pages/api/item/[id].js
@@ -0,0 +1,19 @@
+import connectDB from "../../../utils/database";
+import { ItemModel } from "../../../utils/schemaModels";
+
+const getSingleItem = async (req, res) => {
+  try {
+    await connectDB();
+    const singleItem = await ItemModel.findById(req.query.id);
+    return res.status(200).json({
+      massage: "アイテムの読み取り成功（シングル）",
+      singleItem: singleItem,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      massage: "アイテムの読み取り失敗（シングル）",
+    });
+  }
+};
+
+export default getSingleItem;

0976d590b8ae069ce62a492c7b13acc6b70b6f09
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-03-23 15:16:02 +0900

    ✨ ログイン機能

---
+++ pages/api/user/login.js
@@ -0,0 +1,26 @@
+import connectDB from "../../../utils/database";
+import { UserModel } from "../../../utils/schemaModels";
+
+const loginUser = async (req, res) => {
+  try {
+    await connectDB();
+    const savedUserData = await UserModel.findOne({ email: req.body.email });
+    if (savedUserData) {
+      if (req.body.password === savedUserData.password) {
+        return res.status(200).json({ message: "ログイン成功" });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "ログイン失敗 : パスワードが間違っています" });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: "ログイン失敗 : ユーザー登録をしてください" });
+    }
+  } catch (err) {
+    return res.status(200).json({ message: "ログイン失敗" });
+  }
+};
+
+export default loginUser;

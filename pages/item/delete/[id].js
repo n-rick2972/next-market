dc2c0d9b3c1a16c43b9e6bfe2b88fdd8d71bda8b
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-03-25 19:57:48 +0900

    ✨ ページの表示を制限する機能

--- pages/item/delete/[id].js
+++ pages/item/delete/[id].js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import useAuth from "../../../utils/useAuth";
 
 const DeleteItem = (props) => {
   const handleSubmit = async (e) => {
@@ -22,23 +23,29 @@ const DeleteItem = (props) => {
     }
   };
 
-  return (
-    <div>
-      <h1>アイテム削除</h1>
-      <form onSubmit={handleSubmit}>
-        <h2>{props.singleItem.title}</h2>
-        <Image
-          src={props.singleItem.item}
-          width={750}
-          height={500}
-          alt="item-image"
-        />
-        <h3>${props.singleItem.price}</h3>
-        <p>{props.singleItem.description}</p>
-        <button>削除</button>
-      </form>
-    </div>
-  );
+  const loginUser = useAuth();
+
+  if (loginUser === props.singleItem.email) {
+    return (
+      <div>
+        <h1>アイテム削除</h1>
+        <form onSubmit={handleSubmit}>
+          <h2>{props.singleItem.title}</h2>
+          <Image
+            src={props.singleItem.item}
+            width={750}
+            height={500}
+            alt="item-image"
+          />
+          <h3>${props.singleItem.price}</h3>
+          <p>{props.singleItem.description}</p>
+          <button>削除</button>
+        </form>
+      </div>
+    );
+  } else {
+    return <h1>権限がありません。</h1>;
+  }
 };
 
 export default DeleteItem;
--- pages/item/update/[id].js
+++ pages/item/update/[id].js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useAuth from "../../../utils/useAuth";
 
 const UpdateItem = (props) => {
   const [title, setTitle] = useState(props.singleItem.title);
@@ -33,46 +34,52 @@ const UpdateItem = (props) => {
     }
   };
 
-  return (
-    <div>
-      <h1>アイテム編集</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          type="text"
-          name="title"
-          placeholder="アイテム名"
-          required
-        />
-        <input
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          type="text"
-          name="price"
-          placeholder="価格"
-          required
-        />
-        <input
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-          type="text"
-          name="image"
-          placeholder="画像"
-          required
-        />
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          name="description"
-          rows={15}
-          placeholder="商品説明"
-          required
-        ></textarea>
-        <button>編集</button>
-      </form>
-    </div>
-  );
+  const loginUser = useAuth();
+
+  if (loginUser === props.singleItem.email) {
+    return (
+      <div>
+        <h1>アイテム編集</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            name="title"
+            placeholder="アイテム名"
+            required
+          />
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            type="text"
+            name="price"
+            placeholder="価格"
+            required
+          />
+          <input
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            type="text"
+            name="image"
+            placeholder="画像"
+            required
+          />
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            name="description"
+            rows={15}
+            placeholder="商品説明"
+            required
+          ></textarea>
+          <button>編集</button>
+        </form>
+      </div>
+    );
+  } else {
+    return <h1>権限がありません。</h1>;
+  }
 };
 
 export default UpdateItem;

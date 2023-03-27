dc0848dfe0d79ddbc3114fc9f0145f4bb52b174a
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-03-27 16:31:14 +0900

    🐛 ローカル環境からパブリック環境へURL変更

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,9 @@ const ReadAllItems = (props) => {
 export default ReadAllItems;
 
 export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:3000/api/item/readall");
+  const response = await fetch(
+    "https://next-market-kohl.vercel.appapi/item/readall"
+  );
   const allItems = await response.json();
   return {
     props: allItems,
--- pages/user/login.js
+++ pages/user/login.js
@@ -7,17 +7,20 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/user/login", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
+      const response = await fetch(
+        "https://next-market-kohl.vercel.app/api/user/login",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        }
+      );
       const jsonData = await response.json();
       localStorage.setItem("token", jsonData.token);
       alert(jsonData.message);

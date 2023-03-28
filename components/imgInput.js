54fe38eb61ad0c5c0b48bbf54f338b43314b85c7
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-03-28 13:52:54 +0900

    ✨ 画像の取り込み

---
+++ components/imgInput.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const ImgInput = (props) => {
+  const [imageFile, setImageFile] = useState("");
+
+  const handleClick = async () => {
+    try {
+      const data = new FormData();
+      data.append("file", imageFile);
+      data.append("upload", "lm5v5ted");
+      data.append("cloud_name", "dxn1xs3fq");
+      const response = await fetch(
+        "https://api.cloudinary.com/v1_1/dxn1xs3fq/image/upload",
+        { method: "POST", body: data }
+      );
+      const jsonData = await response.json();
+      await props.setImage(jsonData.url);
+      alert("画像アップロード成功");
+    } catch (err) {
+      alert("アップロード失敗");
+    }
+  };
+  return (
+    <div className="img-input">
+      <input
+        type="file"
+        onChange={(e) => singleFile(e.target.files[0])}
+        accept="image/png, image/jpeg"
+      />
+      <button onClick={handleClick} disabled={!imageFile}>
+        画像 Upload
+      </button>
+    </div>
+  );
+};
+
+export default ImgInput;
